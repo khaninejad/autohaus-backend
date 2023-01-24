a23c2d01ec98a38660c43d1e68c2b9583011c4d7
Payam <payam.khaninejad@coinhaven.com>
2023-01-24 11:23:12 +0100

    change init project dir structure

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const port: number = process.env.PORT
+    ? Number.parseInt(process.env.PORT, 10)
+    : 4000;
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
+  Logger.log(`🚀 application now running at ${port}`, 'main.ts');
 }
-bootstrap();
+
+bootstrap().then(() => Logger.log('🛠 bootstrapped application', 'main.ts'));
