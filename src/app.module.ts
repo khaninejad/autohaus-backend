7d00cfdbbd3e75a108da6b53778fda5cee11605a
Payam <payam.khaninejad@coinhaven.com>
2023-01-26 17:03:26 +0100

    extract db string

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { UserModule } from './user/user.module';
 import { DepartmentModule } from './department/department.module';
 import { EmployeeModule } from './employee/employee.module';
 import { TrackHistoryModule } from './track_history/track_history.module';
+import configuration from './shared/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://0.0.0.0:27017/nest'),
+    MongooseModule.forRoot(configuration().db_string),
     UserModule,
     DepartmentModule,
     EmployeeModule,
--- src/shared/config.ts
+++ src/shared/config.ts
@@ -1,9 +1,11 @@
 export interface ConfigurationOptions {
   secretKey: string;
   expiresIn: string;
+  db_string: string;
 }
 
 export default (): ConfigurationOptions => ({
   secretKey: process.env.SECRETKEY || 'secret',
   expiresIn: process.env.EXPIREIN || '1d',
+  db_string: process.env.DB_STRING || 'mongodb://0.0.0.0:27017/nest',
 });
